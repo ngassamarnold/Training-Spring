f085baeb13ffb74156029d57f404c456d3ffc3c3
Arnold Ngassam <ngassam.arnold@nanobnk.com>
2022-09-27 22:44:09 +0400

    Passer à Spring Boot

--- corre/src/main/java/com/mycompany/dvdstore/App.java
+++ corre/src/main/java/com/mycompany/dvdstore/App.java
@@ -1,18 +1,21 @@
 package com.mycompany.dvdstore;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.mycompany.dvdstore.controller.MovieController;
 
 
+@SpringBootApplication
 public class App 
 {
 
 	public static void main( String[] args )
     {
-		ApplicationContext context= new ClassPathXmlApplicationContext("applicationContext.xml");
-
+		//ApplicationContext context= new ClassPathXmlApplicationContext("applicationContext.xml");
+		//ApplicationContext context= new AnnotationConfigApplicationContext(App.class);
+		ApplicationContext context= SpringApplication.run(App.class);
 		MovieController lController= context.getBean(MovieController.class);
 		
 		lController.addUsingConsle();       
